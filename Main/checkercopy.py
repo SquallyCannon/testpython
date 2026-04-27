c5d2192a8b3fdb577ab7feb909b41c078a031d48
SquallyCannon <malachi.arney@students.cvtech.edu>
2026-04-27 10:40:00 -0500

    Add files via upload

--- Main/checkercopy.py
+++ Main/checkercopy.py
@@ -1,6 +1,5 @@
 import random
 import checkerex
-import math
 game_state = True
 turn = 'Black'
 turnson = True
@@ -21,29 +20,23 @@ toQValid = False
 toPValid = False
 stupiderror = False
 extraturn = 0
-skip = False
-End = False
-br0 = ['0','1 ', '2 ', '3 ', '4 ', '5 ', '6 ', '7 ', '8 ']
-br1 = ['1','BP', '--', 'BP', '--', 'BP', '--', 'BP', '--']
-br2 = ['2','--', 'BP', '--', 'BP', '--', 'BP', '--', 'BP']
-br3 = ['3','BP', '--', 'BP', '--', 'BP', '--', 'BP', '--']
-br4 = ['4','--', '--', '--', '--', '--', '--', '--', '--']
-br5 = ['5','--', '--', '--', '--', '--', '--', '--', '--']
-br6 = ['6','--', 'WP', '--', 'WP', '--', 'WP', '--', 'WP']
-br7 = ['7','WP', '--', 'WP', '--', 'WP', '--', 'WP', '--']
-br8 = ['8','--', 'WP', '--', 'WP', '--', 'WP', '--', 'WP']
-br9 = ['Skip', f"(^v^)~(ovo)~{turn}~(uwu)~(>:3)-_-'", 'End']
-game_board = [br0, br1, br2, br3, br4, br5, br6, br7, br8, br9]
+br0 = ['0 ','1 ', '2 ', '3 ', '4 ', '5 ', '6 ', '7 ', '8 ']
+br1 = ['1 ','BP', '--', 'BP', '--', 'BP', '--', 'BP', '--']
+br2 = ['2 ','--', 'BP', '--', 'BP', '--', 'BP', '--', 'BP']
+br3 = ['3 ','BP', '--', 'BP', '--', 'BP', '--', 'BP', '--']
+br4 = ['4 ','--', '--', '--', '--', '--', '--', '--', '--']
+br5 = ['5 ','--', '--', '--', '--', '--', '--', '--', '--']
+br6 = ['6 ','--', 'WP', '--', 'WP', '--', 'WP', '--', 'WP']
+br7 = ['7 ','WP', '--', 'WP', '--', 'WP', '--', 'WP', '--']
+br8 = ['8 ','--', 'WP', '--', 'WP', '--', 'WP', '--', 'WP']
+game_board = [br0, br1, br2, br3, br4, br5, br6, br7, br8]
 save_board = game_board
 def Printboard():
     global brp
-    global br9
-    global turn
-    br9 = ['Skip', f"(^v^)~(ovo)~{turn}~(uwu)~(>:3)|-_-|", 'End']
-    game_board = [br0, br1, br2, br3, br4, br5, br6, br7, br8, br9]
     for row in game_board:
         print(row)
     print('')
+    print(turn)
     brp = '--'
 def failfunc():
     global Valid
@@ -253,66 +246,45 @@ while game_state == True:
     Printboard()
 
     try:
-        move = [input('peice-y:'),input('peice-x:')]
-        to = [input('to-y:'),input('to-x:')]
-        if move[0] == 'skip' or move[1] == 'skip' or to[0] == 'skip' or to[1] == 'skip':
-            skip = True
-        elif move[0] == 'end' or move[1] == 'end' or to[0] == 'end' or to[1] == 'end':
-            End = True
-        else:
-            move = [int(move[0]),int(move[1])]
-            to = [int(to[0]),int(to[1])]
-            if move[0] in (11,13,15,17,22,24,26,28,31,33,35,37,42,44,46,48,51,53,55,57,62,64,66,68,71,73,75,77,82,84,86,88):
-                move[1] = move[0]%10
-                move[0] = math.floor(move[0]/10)
-            if to[0] in (11,13,15,17,22,24,26,28,31,33,35,37,42,44,46,48,51,53,55,57,62,64,66,68,71,73,75,77,82,84,96,88):
-                to[1] = to[0]%10
-                to[0] = math.floor(to[0]/10)
-            if move[0] > 8 or to[0] > 8 or move[1] > 8 or to[1] > 8:
-                brp = '0'
-                move = [0,0]
-                to = [0,0]
+        move = [int(input('peice-y:')),int(input('peice-z:'))]
+        to = [int(input('to-y:')),int(input('to-z:'))]
+        if move[0] > 8 or to[0] > 8 or move[1] > 8 or to[1] > 8:
+            move = [0,0]
+            to = [0,0]
     except:
         print('Input must be a number 1-8')
-        brp = '0'
         move = [0,0]
         to = [0,0]
+    
+    row = game_board[move[0]]
 
-    if skip == False and End == False:
+
+    if QM == True:
         row = game_board[move[0]]
+        piece = row[move[1]]
 
+        if piece == 'BQ':
+            Black_QM = True
+        elif piece == 'WQ':
+            White_QM = True
 
-        if QM == True:
-            row = game_board[move[0]]
-            piece = row[move[1]]
-
-            if piece == 'BQ':
-                Black_QM = True
-            elif piece == 'WQ':
-                White_QM = True
-
-        if move[0] - to[0] == 2 or move[0] + to[0] == 2:
-            #row = game_board[to[0]-(move[0]-to[0])]
-            capturable = False
-        if not move[0] == to[0]-1 and not move[0] == to[0]+1 or not move[1] == to[1]-1 and not move[1] == to[1]+1:
-                if capturable == False:
-                    move = [0,0]
-                    to = [0,0]
-        if turn == 'Black' and Black_QM == False and not move[0] == to[0]-1:
+    if move[0] - to[0] == 2 or move[0] + to[0] == 2:
+        #row = game_board[to[0]-(move[0]-to[0])]
+        capturable = False
+    if not move[0] == to[0]-1 and not move[0] == to[0]+1 or not move[1] == to[1]-1 and not move[1] == to[1]+1:
             if capturable == False:
                 move = [0,0]
                 to = [0,0]
-        if turn == 'White' and White_QM == False and not move[0] == to[0]+1:
-            if capturable == False:
-                move = [0,0]
-                to = [0,0]
+    if turn == 'Black' and Black_QM == False and not move[0] == to[0]-1:
+        if capturable == False:
+            move = [0,0]
+            to = [0,0]
+    if turn == 'White' and White_QM == False and not move[0] == to[0]+1:
+        if capturable == False:
+            move = [0,0]
+            to = [0,0]
 
-        movement()
-
-    if End == True:
-        Printboard()
-        game_state = False
-
+    movement()
     if Valid == True and stupiderror == False and extraturn == 0:
         if turn == 'White':
             turn = 'Black'
@@ -336,5 +308,4 @@ while game_state == True:
     Black_QM = False
     White_QM = False
     movevalid = False
-    skip = False
     print('')
